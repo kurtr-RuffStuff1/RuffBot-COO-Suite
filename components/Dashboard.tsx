b60f1e80a01df9b55402356f3491f69f117defb5
kurtr-RuffStuff1 <kurtr@ruffstuffcounseling.com>
2026-02-10 13:32:27 -0700

    Clean file and fix JSX escape

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,32 +2,3 @@
 103|              <h3 className="text-sm font-black text-slate-900 uppercase tracking-widest">Operational Efficiency</h3>
 11:39:05.093 
 104|              <span className="text-[10px] font-bold text-emerald-500 bg-emerald-50 px-2 py-1 rounded-md tracking-tighter">Target: &gt;85%</span>
-11:39:05.094 
-   |                                                                                                                                   ^
-11:39:05.094 
-105|            </div>
-11:39:05.094 
-106|            {/* Added relative and min-h to ensure ResponsiveContainer has valid dimensions on mount */}
-11:39:05.094 
-11:39:05.094 
-    at failureErrorWithLog (/vercel/path0/node_modules/esbuild/lib/main.js:1467:15)
-11:39:05.094 
-    at /vercel/path0/node_modules/esbuild/lib/main.js:736:50
-11:39:05.095 
-    at responseCallbacks.<computed> (/vercel/path0/node_modules/esbuild/lib/main.js:603:9)
-11:39:05.095 
-    at handleIncomingPacket (/vercel/path0/node_modules/esbuild/lib/main.js:658:12)
-11:39:05.095 
-    at Socket.readFromStdout (/vercel/path0/node_modules/esbuild/lib/main.js:581:7)
-11:39:05.095 
-    at Socket.emit (node:events:508:28)
-11:39:05.095 
-    at addChunk (node:internal/streams/readable:559:12)
-11:39:05.095 
-    at readableAddChunkPushByteMode (node:internal/streams/readable:510:3)
-11:39:05.096 
-    at Readable.push (node:internal/streams/readable:390:5)
-11:39:05.096 
-    at Pipe.onStreamRead (node:internal/stream_base_commons:189:23)
-11:39:05.146 
-Error: Command "npm run build" exited with 1
