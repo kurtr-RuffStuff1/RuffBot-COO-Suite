d2d02e6137d7d8b8ddaf55de753ebbad4b7ebb56
kurtr-RuffStuff1 <kurtr@ruffstuffcounseling.com>
2026-02-10 13:55:20 -0700

    Fix malformed span tag

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,4 +2,6 @@
   Operational Efficiency
 </h3>
 
-           <span className="text-[10px] font-bold text-emerald-500 bg-emerald-50 px-2 py-1 rounded-md tracking-tighter">Target: &gt;85%</span>
+<span className="text-[10px] font-bold text-emerald-500 bg-emerald-50 px-2 py-1 rounded-md tracking-tighter">
+  Target: &gt;85%
+</span>
